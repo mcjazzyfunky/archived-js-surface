5d38f321bf4c87b8aaeb3d2e8848095c9f30fb67
Funky MC <mcjazzyfunky@gmail.com>
2016-02-14 19:36:52 +0000

    Continued with new prototype

--- demo/demo.js
+++ demo/demo.js
@@ -1,74 +1,246 @@
 'use strict';
 
-alert(1)
-const
-    {Observable, Subject} = window.Rx,
-    React = window.React,
-    ReactDOM = window.ReactDOM;
+import {Observable, Subject} from 'rx';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-
-class Component {
-    constructor(config) {
-        throw new Error('[Component.constructor] Component cannot be instantiated by using the constructor - '
-                + "please use the factory method 'Component.create' instead");
-    }
-    
-    getConfig() {
-        return this.__config;
-    }
-    
-    static create(config) {
-        const typeNameRegex = /[a-zA-Z][a-zA-Z0-9.]*/; 
+export const Component = {
+    createFactory(config) {
+        const configError = checkComponentClassConfig(config);
         
-        if (config === null || typeof config !== 'object') {
-            throw new TypeError('[Component.create] Component configuration has to of type {typeName : String, view: Function}');
-        } else if (config.typeName === undefined  || config.typeName === null) {
-            throw new TypeError("[Component.create] Component configuration value 'typeName' is missing");
-        } else if (typeof config.typeName !== 'string' || !config.typeName.match(typeNameRegex)) {
-            throw new TypeError(`[Component.create] Illegal configuration value 'typeName' (must match regex ${typeNameRegex})`);
-        } else if (config.view === undefined || config.view === null) {
-            throw new TypeError("[Component.create] Component configuration value 'view' is missing");
-        } else if (typeof config.view === 'function') {
-            throw new TypeError("[Component.create] Component configuration value 'view' has to be a function");
+        if (configError) {
+            throw new TypeError('[Component.create] ' + configError.getMessage());
+        } 
+        
+        const ret = (initialProps, children) => ({
+            isBlingComponent: true,
+            classId: config.classId,
+            initialProps,
+            children
+        });
+        
+        ret.getConfig = () => {
+            return config;
         }
-
-        const ret = function () {
-            this.__config = Object.freeze({
-                typeName: config.typeName || null,
-                view: config.view
-            });
-        };
         
-        ret.prototype = Object.create(Component.prototype);
         return ret;
     }
 }
 
+export class ComponentAdapter {
+    convertComponentClass(componentClass) {
+        throw new Error(
+            '[ComponentAdapter:convertComponentClass] '
+            + 'Method not implemented/overridden');        
+    }
+}
+
+function checkComponentClassConfig(config) {
+    var ret;
+    
+    const classIdRegex = /^[A-Z][a-z0-9]*$/;
+    
+    if (config === null || typeof config !== 'object') {
+        ret = new TypeError('Component configuration has to of type {classId : String, view: Function}');
+    } else if (config.classId === undefined  || config.classId === null) {
+        ret = new TypeError("Component configuration value 'classId' is missing");
+    } else if (typeof config.classId !== 'string' || !config.classId.match(classIdRegex)) {
+        ret = new TypeError(`Illegal value for 'typeID' (must match regex ${classIdRegex})`);
+    } else if (config.view === undefined || config.view === null) {
+        ret = new TypeError("Component configuration value 'view' is missing");
+    } else if (typeof config.view !== 'function') {
+        ret = new TypeError("Component configuration value 'view' has to be a function");
+    } else {
+        ret = null;
+    }
+    
+    return ret;
+}
+
+
+class ComponentMgr {
+    constructor() {
+        this.__componentClassRegistry = new Map();
+        this.__adapterRegistry = new Map();
+        this.__convertRegistry = new Map();
+    }
+
+    registerComponentClass(componentClass) {
+        if (componentClass === null || typeof componentClass !== 'function' || typeof componentClass.getConfig !== 'function'
+                || checkComponentClassConfig(componentClass.getConfig())) {
+            throw new TypeError(
+                    '[ComponentMgr.registerComponent] '
+                    + "First argument must be a proper component class created by 'Component.createClass'");
+        }
+       
+        const config = componentClass.getConfig();
+        
+        if (this.__componentClassRegistry.has(config.classId)) {
+            throw new Error(
+                "[CompnentMgr:registerComponent] Component with class id '"
+                + config.classId
+                + "' has already been registered");
+        }
+
+        this.__componentClassRegistry.set(config.classId, config);
+    }
+    
+    registerAdapter(adapterId, adapter) {
+        const adapterIdRegex = /^[A-Z][a-zA-Z0-9]*$/;
+        
+        if (typeof  adapterId !== 'string' || adapterId.match(adapterIdRegex)) {
+            throw new TypeError('[ComponentMgr:registerAdapter] '
+                + 'First argument must be a proper adapter ID '
+                + 'that matches regex '
+                + adapterIdRegex);
+        } else if (this.__adapterRegistry.has(adapterId)) {
+            throw new Error(
+                "[CompnentMgr:registerAdapter] Adapter with  "
+                + adapterId
+                + "' has already been registered");
+        }
+        
+        this.__adapterRegistry.set(adapterId, adapter);
+    }
+    
+    getComponentClass(classId) {
+        const componentClass = this.__componentClassRegistry.get(classId);
+        
+        if (!componentClass) {
+            throw new Error(
+                "[ComponentMgr:getComponentConfig] Component with class id '"
+                + classId
+                + "' has not been registered");
+        }
+        
+        return componentClass;
+    }
+    
+    convertComponentClass(classId, adapterId) {
+        const convertId = `${classId}/${adapterId}`;
+        
+        let ret = this.__convertRegistry.get(convertId);
+
+        if (!ret) {
+            const
+                componentClass = this.__componentClassRegistry.get(classId),
+                adapter = this.__adapterRegistry.get(adapterId);
+
+            if (!componentClass) {
+                throw new Error(
+                    "[ComponentMgr:convertComponent] Component with class id '"
+                    + classId
+                    + "' has not been registered");
+            } else if (!adapter) {
+                throw new Error(
+                    "[ComponentMgr:convertComponent] Adapter with id '"
+                    + adapterId
+                    + "' has not been registered");
+            }
+            
+            ret = adapter.convertComponentClass(componentClass);
+            this.__convertRegistry.set(convertId, ret);
+        }
+        
+        return ret; 
+    }
+    
+    isComponentClassRegistered(classId) {
+        return this.__componentClassRegistry.has(classId);
+    }
+    
+    static getGlobal() {
+        let ret = this.__globalInstance;
+        
+        if (!ret) {
+            ret = this.__globalInstance = new ComponentMgr();
+        }
+        
+        return ret;
+    }
+}
+
+class EventMgr {
+    constructor() {
+        const subjectsByName = new Map();
+    
+        this.on = (eventName) => {
+            let ret = subjectsByName.get(eventName);
+            
+            if (!ret) {
+                ret = new Subject(); 
+                subjectsByName.set(eventName, ret);
+            }
+            
+            return ret.asObserver();
+        }    
+        
+        this.bind = (eventName) => {
+            let ret = subjectsByName.get(eventName);
+
+            if (!ret) {
+                ret = new Subject(); 
+                subjectsByName.set(eventName, ret);
+            }
+            
+            return ret.asObservable();
+        }
+    }
+    
+    on(eventName) {
+        throw Error('[EventMgr:on] Method not implemented');
+    }
+    
+    bind(eventName) {
+        throw Error('[EventMgr:bind] Method not implemented');
+    }
+}
+
+export const button = Component.createClass({
+    classId: 'Button',
+
+    defaultProps: {
+        caption: ''
+    },
+    
+    view(dom, propsObs) {
+        return {
+            display: propsObs.map(props => {
+                dom.button(
+                    {class: 'component'},
+                    props.get('caption'));
+            })
+        };
+    }
+});
 
 // [-] 42 [+]
-export default Component.create({
-    typeId: 'counter',
+export const counter = Component.createClass({
+    classId: 'Counter',
     
     defaultProps: {
         caption: ''  
     },
     
-    view: (dom, {on, bind}, propsObs) => {
+    view: (dom, propsObs) => {
         const
+            {on, bind} = new EventMgr(),
+
             plusObs = on('plusButtonClicked')
                     .map(_ => 1),
 
             minusObs = on('minusButtonClicked')
                     .map(_ => -1),
 
-            counterObs = Observable.merge(plusObs, minusObs)
-                    .startWith(0)
+            counterObs = //Observable.merge(plusObs, minusObs)
+                    plusObs.startWith(0)
                     .scan((prev, curr) => prev + curr);
 
         return {
-            display: propsObs.merge(counterObs, (props, counter) =>
+            display: propsObs.map(props =>
                     dom.div(
                         {className: 'ui-counter'},
+                        
                         dom.label(
                             {className: 'ui-counter-label'},
                             props.get('caption')
@@ -81,7 +253,7 @@ export default Component.create({
                         ),
                         dom.span(
                             {className: 'ui-counter-value'},
-                             counter
+                             "counter"
                         ),
                         dom.button({
                                 className: 'ui-counter-increment',
@@ -97,64 +269,31 @@ export default Component.create({
     }
 });
 
+class ReactAdapter extends ComponentAdapter {
+    convertComponentClass(componentClass) {
+        if (typeof componentClass !== 'function'
+                || typeof componentClass.getConfig !== 'function'
+                || checkComponentClassConfig(componentClass.getConfig())) {
 
-
-class Counter extends Component {
-    constructor() {
-        super({
-            typeName: 'Counter',
-            
-            view: (dom, interaction, propsObs, dependenciesObs) => {
-                const
-                    {on, bind}  = interaction,
+            throw new TypeError(
+                    '[ReactAdapter.convertComponentClass] '
+                    + 'First argument must be a proper component class created by '
+                    + "'Component.createClass'");
+        }
         
-                    plusObs = on('plusButtonClicked')
-                            .map(_ => 1),
-        
-                    minusObs = on('minusButtonClicked')
-                            .map(_ => -1),
-        
-                    stateObs = Observable.merge(plusObs, minusObs)
-                            .startWith(0)
-                            .scan((prev, curr) => prev + curr), 
-        
-                    uiTreeObs = propsObs.merge(propsObs, (props, state) =>
-                            dom.div(
-                                null,
-                                dom.button({ onClick: bind('minusButtonClicked') }, '+'),
-                                dom.label(null, state),
-                                dom.button({ onClick: bind('minusButtonClicked') }, '-'))),
-            
-                    uiEvents = {
-                        update: stateObs.map(state => {counter: state})
-                    };
-                    
-                return {
-                    uiTree: uiTreeObs,
-                    uiEvents
-                };
-            }
-        });
-    } 
-}
-
-
-
-
-
-
-
-class ComponentAdapter {
-    convertComponent(component) {
-         throw Error('[ComponentAdapter:convertComponent] Method has not been implemented/overridden');
+        const constructor = function () {};
+        constructor.prototype = new ReactComponent(componentClass);
+        return React.createFactory(constructor);
     }
 }
 
-class ReactAdapter extends ComponentAdapter {
-    convertComponent(component) {
-        const constructor = function () {};
-        constructor.prototype = new ReactComponent(component);
-        return React.createFactory(constructor);
+class Reader {
+    constructor(obj) {
+        this.__obj = obj;
+    }
+    
+    get(key) {
+        return this._obj[key];
     }
 }
 
@@ -162,6 +301,8 @@ const domBuilder = {};
 
 class ReactComponent extends React.Component {
     constructor(component) {
+        super();
+        
         const interaction = {
            on: () => new Observable(),
            bind: () => new Subject()
@@ -171,13 +312,13 @@ class ReactComponent extends React.Component {
         this.__propsSbj = new Subject();
         this.__subscriptionViewDisplay = null;
         this.__subscriptionViewEvents = null;
-        
-        const view = this._component.getView(
+        console.log(this.__component);
+        const view = this.__component.getConfig().view(
             domBuilder,
             interaction,
             this.__propsSbj);
-        
-        this.__viewDisplayObs = view;
+
+        this.__viewDisplayObs = view.display;
         this.__viewEvents = view.events;
 
     }
@@ -185,8 +326,8 @@ class ReactComponent extends React.Component {
     componentWillMount() {
         this.__viewDisplayObs.subscribe(domTree => {
             this.__domTree = domTree;
-            this.forceUpate();
-            this.__domTree = null;
+            this.forceUpdate();
+//            this.__domTree = null;
         });
     }
     
@@ -196,28 +337,29 @@ class ReactComponent extends React.Component {
     
     componentWillReceiveProps(nextProps) {
         this.props = nextProps();
-        this.__propsSbj.next(nextProps);
+        this.__propsSbj.onNext(nextProps);
         this.forceUpdate();
     }
     
-    componentShouldUpdate() {
+    shouldComponentUpdate() {
         return false;
     }
     
     render() {
-        return this._domTree;
+        return this.__domTree;
     }
 }
 
 
 const
-    component = new Counter(),
     adapter = new ReactAdapter(),
-    ReactCounter = adapter.convertComponent(component);
+    reactCounter = adapter.convertComponent(counter);
 
 
+setTimeout(() => {
+    ReactDOM.render(React.createElement('div', null, reactCounter(null, 'My counter:')), document.getElementById('content'));
+}, 0);
 
-ReactDOM.render(React.createElement('div', null, ReactCounter()), document.getElementById('content'))
 
 
 const tagNames = [
